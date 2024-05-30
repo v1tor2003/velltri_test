0bc9791bea81a3ef48905c4495b34f945dae5532
v1tor2003 <vitor.pr04@hotmail.com>
2024-05-30 18:44:01 -0300

    Added bar chart for football players

---
+++ app/chartsData.ts
@@ -0,0 +1,32 @@
+export const playerData = [
+  {
+    "player": "C. Ronaldo",
+    "goals": 44,
+    "assists": 13,
+    "penalties": 9,
+  },
+  {
+    "player": "K. Mbappé",
+    "goals": 44,
+    "assists": 10,
+    "penalties": 10,
+  },
+  {
+    "player": "H. Kane",
+    "goals": 44,
+    "assists": 12,
+    "penalties": 8,
+  },
+  {
+    "player": "V. Gyokeres ",
+    "goals": 43,
+    "assists": 15,
+    "penalties": 8,
+  },
+  {
+    "player": "E. Haaland",
+    "goals": 38,
+    "assists": 6,
+    "penalties": 9,
+  },
+] 
